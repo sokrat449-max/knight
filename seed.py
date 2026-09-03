fb471af6b8c15d9200141f073e8d86de4dd2b183
sokrat449-max <sokrat449-max@github.com>
2026-09-03 19:05:17 +0300

    autonomous memory and evolution gen 16

--- seed.py
+++ seed.py
@@ -113,10 +113,10 @@ class SeedNode:
             return False
         with open(self.script_file, "r") as f:
             code = f.read()
-        if "EVOLUTION_LOG: gen_15
-            code = re.sub(r'EVOLUTION_LOG: gen_15
+        if "EVOLUTION_LOG: gen_16
+            code = re.sub(r'EVOLUTION_LOG: gen_16
         else:
-            code = code.replace("class SeedNode:", f"# EVOLUTION_LOG: gen_15
+            code = code.replace("class SeedNode:", f"# EVOLUTION_LOG: gen_16
         with open(self.script_file, "w") as f:
             f.write(code)
         self.state["code_mutations"] += 1
